4c6ca211652c6c0282561c775fbf444758ce9c7a
rijalganzz <keyshan430@gmail.com>
2025-07-18 22:24:16 +0700

    Add files via upload

--- api/download/SoundCloud.js
+++ api/download/SoundCloud.js
@@ -1,18 +1,5 @@
-async function videy(url) {
-  try {
-    let id = url.split("id=")[1]
-    let typ = '.mp4'
-    if (id.length === 9 && id[8] === '2') {
-      typ = '.mov';
-    }
-    return `https://cdn.videy.co/${id + typ}`
-  } catch (error) {
-    console.error(`Error fetching the URL: ${error.message}`);
-  }
-}
-
 module.exports = function (app) {
-app.get('/download/videy', async (req, res) => {
+app.get('/download/soundcloud', async (req, res) => {
         try {
             const { apikey } = req.query;
             if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
@@ -20,10 +7,10 @@ app.get('/download/videy', async (req, res) => {
             if (!url) {
             return res.json({ status: false, error: 'Url is required' });
             }
-            const results = await videy(url);
+            const results = await global.fetchJson(`https://fastrestapis.fasturl.cloud/downup/soundclouddown?url=${url}`)
             res.status(200).json({
                 status: true,
-                result: results
+                result: results.result
             });
         } catch (error) {
             res.status(500).send(`Error: ${error.message}`);
